f7b6f0535de064fad3150bb7715f2fea0dffa7a4
lv <olv@foxmail.com>
2016-08-04 10:39:24 +0800

    有的没有完成

---
+++ LeetCode/src/easy/NO371.java
@@ -0,0 +1,21 @@
+package easy;
+
+import org.junit.Test;
+
+/**
+ * 371. Sum of Two Integers
+ *	Calculate the sum of two integers a and b, 
+ *	but you are not allowed to use the operator + and -.
+ *	
+ *	Example:
+ *	Given a = 1 and b = 2, return 3.
+ */
+public class NO371 {
+	@Test
+	public void test() {
+	}
+
+	public int getSum(int a, int b) {
+		return 0;
+	}
+}
