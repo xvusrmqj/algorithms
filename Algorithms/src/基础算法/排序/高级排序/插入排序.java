2f2a9afb3bc7b301775507650b433233ced3fa78
lv <olv@foxmail.com>
2016-08-18 22:25:11 +0800

    DONE

--- Algorithms/src/基础算法/排序/高级排序/插入排序.java
+++ Algorithms/src/基础算法/排序/高级排序/插入排序.java
@@ -1,10 +1,50 @@
 package 基础算法.排序.高级排序;
 
+import java.util.Arrays;
+
+import org.junit.Test;
+
 public class 插入排序 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test
+	public void test() {
+		//		1. null
+		fun(null);
+		//		2. {}
+		fun(new int[]{});
+		//		3. 无重，无序
+		fun(new int[]{3,2,5,7,8,1});
+		//		5. 无重，正序‘
+		fun(new int[]{2,3,4,5,6});
+		//		4. 无重，反序
+		fun(new int[]{6,5,4,3,2,1});
+		//		5. 有重，无序‘
+		fun(new int[]{2,3,2,3,4,7,6});
+		//		5. 有重，正序‘
+		fun(new int[]{2,2,2,3,4,5,6});
+		//		6. 有重，反序
+		fun(new int[]{6,5,4,3,3,2,2});
 	}
 
+	/**
+	 * 变量及其范围
+	 * i: [1, n-1]
+	 * j: [i-1, 0]
+	 * @param arr
+	 */
+	public void fun(int[] arr) {
+		if(arr==null||arr.length==0) return;
+		System.out.println("排序前：" + Arrays.toString(arr));
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (arr[j] < arr[j-1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = tmp;
+				}else
+					break;
+			}
+		}
+		System.out.println("排序后：" + Arrays.toString(arr));
+		System.out.println("----------------------");
+	}
 }
--- Algorithms/src/基础算法/查找/二分查找.java
+++ Algorithms/src/基础算法/查找/二分查找.java
@@ -1,7 +1,68 @@
 package 基础算法.查找;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class 二分查找 {
-	public static void main(String[] args) {
-		
+	@Test
+	public void test() {
+		//1. 无重， 查找成功
+		assertEquals(fun(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 1), //input
+				0);//excepted: 0
+
+		//2. 无重，查找失败
+		assertEquals(fun(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 0), //input
+				-1);//excepted:-1
+
+		//3. 有重，查找成功
+		assertEquals(fun(new int[] { 1, 1, 3, 4, 5, 6, 7, 8, 9, 10 }, 1), //input
+				1);//excepted:1
+
+		//4. 有重，查找失败
+		assertEquals(fun(new int[] { 1, 1, 3, 4, 5, 6, 7, 8, 9, 10 }, 0), //input
+				-1);//excepted:-1
+
+		//5. null
+		assertEquals(fun(null, 1), //input
+				-1);//excepted:-1
+
+		//6. {}
+		assertEquals(fun(new int[] {}, 1), //input
+				-1);//excepted:-1
+
+	}
+
+	/**
+	 * 二分查找
+	 * 1. 需要几个变量? 分别表示什么?
+	 * 	 - begin  : 范围起始点
+	 * 	 - end	  : 范围结束点
+	 *   - medium : 判断点
+	 * 2. 每个变量的范围是多少?
+	 * 	 - begin = begin 或  medium+1		(arr[medium]>target)
+	 *   - end = end 或 medium -1		(arr[medium]<target)
+	 *   - medium = (begin+end)/2
+	 * @param arr
+	 * @param target
+	 * @return
+	 */
+	private int fun(int[] arr, int target) {
+		if (arr == null || arr.length == 0)
+			return -1;
+		int begin = 0;
+		int end = arr.length - 1;
+		int medium = (begin + end) / 2;
+		while (begin <= end) {
+			if (arr[medium] == target) {
+				return medium;
+			} else if (arr[medium] < target) {
+				begin = medium + 1;
+				medium = (begin + end) / 2;
+			} else {
+				end = medium - 1;
+				medium = (begin + end) / 2;
+			}
+		}
+		return -1;
 	}
 }
