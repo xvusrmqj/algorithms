d22e262b5969a349d711cd7fda7d6346afc8d053
lv <olv@foxmail.com>
2016-08-31 20:35:26 +0800

    添加新方法

--- CodeProblems/src/编程之美/NO2_1.java
+++ CodeProblems/src/编程之美/NO2_1.java
@@ -16,35 +16,51 @@ public class NO2_1 {
 		System.out.println(s);
 		String s2 = Integer.toBinaryString(-1 >> 1);
 		System.out.println(s2);
+		String s3 = Integer.toBinaryString(-1 >>> 1);
+		System.out.println(s3);
 
 	}
 
 	@Test
-	public void test() {
-		Assert.assertEquals(0, count(0));
-		Assert.assertEquals(1, count(1));
-		Assert.assertEquals(1, count(2));
-		Assert.assertEquals(2, count(3));
-		Assert.assertEquals(1, count(4));
-		Assert.assertEquals(2, count(5));
+	public void testCount1() {
+//		Assert.assertEquals(0, count1(0));
+//		Assert.assertEquals(1, count1(1));
+//		Assert.assertEquals(1, count1(2));
+//		Assert.assertEquals(2, count1(3));
+//		Assert.assertEquals(1, count1(4));
+//		Assert.assertEquals(2, count1(5));
+//		Assert.assertEquals(32, count1(-1));
+//		Assert.assertEquals(31, count1(-2));
+//		Assert.assertEquals(31, count1(-3));
 		
-		Assert.assertEquals(2, count(-1));
-		Assert.assertEquals(2, count(-2));
-		Assert.assertEquals(3, count(-3));
+		Assert.assertEquals(0, count2(0));
+		Assert.assertEquals(1, count2(1));
+		Assert.assertEquals(1, count2(2));
+		Assert.assertEquals(2, count2(3));
+		Assert.assertEquals(1, count2(4));
+		Assert.assertEquals(2, count2(5));
+		Assert.assertEquals(32, count2(-1));
+		Assert.assertEquals(31, count2(-2));
+		Assert.assertEquals(31, count2(-3));
 	}
-
-	int count(int i) {
-		boolean fushu = false;
-		if (i < 0)
-			fushu = true;
-		i = Math.abs(i);
+	//向右移位, 需要判断所有位
+	int count1(int i) {
 		int sum = 0;
 		while (i != 0) {
 			if ((i & 1) == 1)
 				sum++;
-			i >>= 1;
+			i >>>= 1;//注意负数向右移位的问题,应该使用无符号右移.
 		}
-		return fushu?sum+1:sum;
+		return sum;
+	}
+	//只需判断1的个数.
+	int count2(int i){
+		int sum = 0;
+		while(i!=0){
+			i&=i-1;
+			sum++;
+		}
+		return sum;
 	}
 
 }
