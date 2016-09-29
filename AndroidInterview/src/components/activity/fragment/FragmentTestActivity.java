6b14599e88fb1717ee7f835cb2dab3f742887e1e
lv <olv@foxmail.com>
2016-09-29 22:41:24 +0800

    add fragment module. but not complete!

---
+++ AndroidInterview/src/components/activity/fragment/FragmentTestActivity.java
@@ -0,0 +1,34 @@
+package components.activity.fragment;
+
+import com.example.androidinterview.R;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FragmentTestActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		final ViewGroup root = (ViewGroup) getLayoutInflater().inflate(R.layout.activity_main, null);
+		Button btn = new Button(this);
+		btn.setText("start a fragment");
+		btn.setId(1000);
+		btn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				FragmentManager manager = getFragmentManager();
+				manager.beginTransaction()//
+						.add(root.getId(), new MyFragment1())//
+						.commit();
+			}
+		});
+
+		root.addView(btn);
+		setContentView(root);
+	}
+}
---
+++ AndroidInterview/src/components/activity/fragment/MyFragment1.java
@@ -0,0 +1,17 @@
+package components.activity.fragment;
+
+import com.example.androidinterview.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment1 extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_my1, container, false);
+		return view;
+	}
+}
