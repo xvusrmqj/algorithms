699eae13318e9b0205a88b52d66dffac135a051d
xvusrmqj <olv@foxmail.com>
2018-12-01 12:49:03 +0800

    add

---
+++ CodeProblems/src/main/java/leetcode_cn/NO1.java
@@ -0,0 +1,41 @@
+package leetcode_cn;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的 两个 整数。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class NO1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int sec = target - nums[i];
+            if (map.containsKey(sec) && map.get(sec) != i) {
+                return new int[]{i, map.get(sec)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    @Test
+    public void test() {
+        int[] ints = twoSum(new int[]{3, 2, 4}, 6);
+        System.out.println(Arrays.toString(ints));
+    }
+}
